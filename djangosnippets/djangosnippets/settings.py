622e254495b09704651b4961e61d1b8077d3f917
katkatprog <katkatprog@gmail.com>
2021-12-17 08:46:20 +0900

    end 1.2.2

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snippets.apps.Snippets'
+    'snippets.apps.SnippetsConfig'
 ]
 
 MIDDLEWARE = [
