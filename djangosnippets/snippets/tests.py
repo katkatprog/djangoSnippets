3b53bbe3ae08754c6f8df9e6e095fcc6df7bcbf8
katkatprog <katkatprog@gmail.com>
2021-12-17 19:14:37 +0900

    end 1.2

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,7 +1,9 @@
+from django.shortcuts import resolve_url
 from django.test import TestCase
 from django.http import HttpRequest, request, response
-from snippets.views import top
+from snippets.views import top, snippet_new, snippet_detail, snippet_edit
 # Create your tests here.
+from django.urls import resolve
 
 class TopPageViewTest(TestCase):
     def test_top_returns_200(self):
@@ -13,3 +15,27 @@ class TopPageViewTest(TestCase):
         request = HttpRequest()
         response = top(request)
         self.assertEqual(response.content, b"Hello World")
+
+class TopPageTest(TestCase):
+    def test_top_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b"Hello World")
+
+class CreateSnippetsTest(TestCase):
+    def test_should_resolve_snippet_new(self):
+        found = resolve("/snippets/new/")
+        self.assertEqual(snippet_new, found.func)
+
+class SnippetDetailTest(TestCase):
+    def test_should_resolve_snippet_detail(self):
+        found = resolve("/snippets/1/")
+        self.assertEqual(snippet_detail, found.func)
+
+class EditSnippetTest(TestCase):
+    def test_should_resolve_snippet_edit(self):
+        found = resolve("/snippets/1/edit/")
+        self.assertEqual(snippet_edit, found.func)
