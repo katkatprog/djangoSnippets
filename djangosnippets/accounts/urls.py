8008069a0e2d2959cc728fee92301f53cce17241
katkatprog <katkatprog@gmail.com>
2021-12-19 17:47:51 +0900

    end 1.6

--- djangosnippets/accounts/urls.py
+++ djangosnippets/accounts/urls.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('login/', LoginView.as_view(
@@ -7,4 +9,9 @@ urlpatterns = [
         template_name = 'accounts/login.html'
     ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', CreateView.as_view(
+        template_name = 'accounts/signup.html',
+        form_class = UserCreationForm,
+        success_url = '/',
+    ), name='signup'),
 ]
